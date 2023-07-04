8856efeb2b2d4e9a8af175d09a21c4c79cba97cc
ElizabethBeaton <elizabethbeaton23@outlook.com>
2023-07-04 23:29:04 +0100

    added logical operators to new card function

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
@@ -20,6 +18,11 @@ function getRandomCard() {
   }
 }
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  let cards = [firstCard, secondCard];
+  let sum = firstCard + secondCard;
   renderGame();
 }
 
@@ -42,9 +45,10 @@ function renderGame() {
 }
 
 function newCard() {
-  console.log("Drawing a new card from the deck!");
-  let card = getRandomCard();
-  sum += card;
-  cards.push(card);
-  renderGame();
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }
